116cbea58e9c77d4f8ee7f855a8e332c0ff71bd4
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-03-02 18:11:53 +0000

    redoing kinematics to be correct
    kinematics were wrong so redid them

--- matrix_mux.m
+++ matrix_mux.m
@@ -1,14 +1,17 @@
-syms theta1 theta2 theta3 theta4 theta5 l2 l3 l4 
+syms theta1 theta2 theta3 theta4 theta5 d1 a2 a3 d5 
 
-T_01 = [cos(theta1) -sin(theta1) 0 0; sin(theta1) cos(theta1) 0 0; 0 0 1 0; 0 0 0 1];
+T_01 = [cos(theta1) 0 -sin(theta1) 0; sin(theta1) 0 cos(theta1) 0; 0 -1 0 d1; 0 0 0 1];
 
-T_12 = [cos(theta2) -sin(theta2) 0 0; 0 0 -1 0; sin(theta2) cos(theta2) 0 0; 0 0 0 1];
+T_12 = [cos(theta2) -sin(theta2) 0 a2*cos(theta2); sin(theta2) cos(theta2) 0 a2*sin(theta2); 0 0 1 0; 0 0 0 1];
 
-T_23 = [cos(theta3) -sin(theta3) 0 l2; sin(theta3) cos(theta3) 0 0; 0 0 1 0; 0 0 0 1];
+T_23 = [cos(theta3) -sin(theta3) 0 a3*cos(theta3); sin(theta3) cos(theta3) 0 a3*sin(theta3); 0 0 1 0; 0 0 0 1];
 
-T_34 = [cos(theta4) -sin(theta4) 0 l3; sin(theta4) cos(theta4) 0 0; 0 0 1 0; 0 0 0 1];
+T_34 = [cos(theta4) 0 -sin(theta4) 0; sin(theta4) 0 cos(theta4) 0; 0 -1 0 0; 0 0 0 1];
 
-T_45 = [cos(theta5) -sin(theta5) 0 l4; 0 0 1 0; -cos(theta5) -sin(theta5) 0 0; 0 0 0 1];
+T_45 = [cos(theta5) -sin(theta5) 0 0; sin(theta5) cos(theta5) 0 0; 0 0 1 d5; 0 0 0 1];
 
 T_05 = T_01*T_12*T_23*T_34*T_45;
-M = simplify(T_05)
+M = simplify(T_05);
+RHS = T_12*T_23*T_34*T_45;
+
+LHS = simplify((inv(T_01))*T_05)
