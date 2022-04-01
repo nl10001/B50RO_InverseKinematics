771c10d9e54b5682f9d97b00e6a725c92b065f8a
nl10001 <98903190+nl10001@users.noreply.github.com>
2022-04-01 19:11:50 +0100

    Add files via upload

--- forward_kinematics.m
+++ forward_kinematics.m
@@ -1,4 +1,4 @@
-syms theta1 theta2 theta3 theta4 theta5 L1 L2 L3 L5
+syms theta1 theta2 theta3 theta4 theta5 L1 L2 L3 L5 r11 r12 r13 px r21 r22 r23 py r31 r32 r33 pz 
 
 T_01 = [cos(theta1) 0   -sin(theta1)  0;
         sin(theta1) 0   cos(theta1)   0;
@@ -27,6 +27,7 @@ T_45 = [cos(theta5) -sin(theta5) 0 0;
 
 T_05 = simplify(T_01*T_12*T_23*T_34*T_45);
 
+T_NN = [r11 r12 r13 px; r21 r22 r23 py; r31 r32 r33 pz; 0 0 0 1];
 
 LHS = simplify(inv(T_01)*T_NN)
 RHS = simplify(T_12*T_23*T_34*T_45) 
